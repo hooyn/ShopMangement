6d0ec4d6759e29c4b3a61d7abdc5e15cfb51f45d
hooyn_twim <hooyn_industry@naver.com>
2022-08-04 08:51:50 +0900

    Initial commit

---
+++ src/test/java/hyjung/shop_mangement/ShopMangementApplicationTests.java
@@ -0,0 +1,13 @@
+package hyjung.shop_mangement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopMangementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
